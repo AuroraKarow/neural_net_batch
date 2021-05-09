7cba6aa6d344fd9ded66a9709ca2cc166f8790ce
AuroraKarow <34509899+AuroraKarow@users.noreply.github.com>
2021-05-10 03:28:21 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "pch.h"
+
+using namespace std;
+using namespace Matrix;
+using namespace batnet;
+using namespace dataset;
+using namespace csvio;
+
+int main(int argc, char *argv[], char *envp[])
+{
+    return EXIT_SUCCESS;
+}
